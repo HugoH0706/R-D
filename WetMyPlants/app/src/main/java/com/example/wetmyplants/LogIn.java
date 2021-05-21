ee651af6223ae20e75ea5b678db1ac542ef645b6
HugoH0706 <hugohakkenberg@hotmail.nl>
2021-05-21 14:58:38 +0100

    navigatie tussen home en login

--- WetMyPlants/app/src/main/java/com/example/wetmyplants/LogIn.java
+++ WetMyPlants/app/src/main/java/com/example/wetmyplants/LogIn.java
@@ -2,6 +2,7 @@ package com.example.wetmyplants;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Intent;
         import android.os.Bundle;
 
         import com.example.wetmyplants.databinding.ActivityMainBinding;
@@ -16,20 +17,22 @@ package com.example.wetmyplants;
 
 public class LogIn extends AppCompatActivity {
 
-    Button b1, b2;
+    Button loginButton, registerButton, homeButton;
     EditText ed1, ed2;
     TextView tx1, tx2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
 
-        b1 = (Button)findViewById(R.id.button2);                        //Login button
+        loginButton = (Button)findViewById(R.id.button2);                        //Login button
+        registerButton = (Button)findViewById(R.id.button3);
+        homeButton = (Button)findViewById(R.id.button);
         ed1 = (EditText)findViewById(R.id.editTextTextEmailAddress);    //email testbalk
         ed2 = (EditText)findViewById(R.id.editTextTextPassword);        //password balk
 
-        b1.setOnClickListener(new View.OnClickListener() {              //dit regelt wat er gebeurd als je op Login knop klikt
+        loginButton.setOnClickListener(new View.OnClickListener() {              //dit regelt wat er gebeurd als je op Login knop klikt
             public void onClick(View v){
                 if(ed1.getText().toString().equals("test@outlook.com") &&    //uiteindelijk nog deze twee string veranderen naar de daadwerkelijke email en wachtwoord
                         ed2.getText().toString().equals("password")){
@@ -37,6 +40,13 @@ public class LogIn extends AppCompatActivity {
                 } else{
                     Toast.makeText(getApplicationContext(), "Wrong email/password...", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent(LogIn.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
--- WetMyPlants/app/src/main/java/com/example/wetmyplants/MainActivity.java
+++ WetMyPlants/app/src/main/java/com/example/wetmyplants/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wetmyplants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,9 +12,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button signinButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_main);
+
+        signinButton = (Button)findViewById(R.id.button4);
+
+        signinButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
     }
 }
