b1e42f7a8b0798b84f0160a83252d2259ffaa5ee
HugoH0706 <hugohakkenberg@hotmail.nl>
2021-05-24 14:10:56 +0100

    Login/register gegevens zijn opgeslagen in app

--- WetMyPlants/app/src/main/java/com/example/wetmyplants/LogIn.java
+++ WetMyPlants/app/src/main/java/com/example/wetmyplants/LogIn.java
@@ -2,7 +2,9 @@ package com.example.wetmyplants;
 
         import androidx.appcompat.app.AppCompatActivity;
 
+        import android.content.Context;
         import android.content.Intent;
+        import android.content.SharedPreferences;
         import android.os.Bundle;
 
         import com.example.wetmyplants.databinding.ActivityMainBinding;
@@ -18,8 +20,10 @@ package com.example.wetmyplants;
 public class LogIn extends AppCompatActivity {
 
     Button loginButton, registerButton, homeButton;
-    EditText ed1, ed2;
+    SharedPreferences sp;
+    EditText email, password;
     TextView tx1, tx2;
+    String emailStr, passwordStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +33,33 @@ public class LogIn extends AppCompatActivity {
         loginButton = (Button)findViewById(R.id.button2);                        //Login button
         registerButton = (Button)findViewById(R.id.button3);
         homeButton = (Button)findViewById(R.id.button);
-        ed1 = (EditText)findViewById(R.id.editTextTextEmailAddress);    //email testbalk
-        ed2 = (EditText)findViewById(R.id.editTextTextPassword);        //password balk
+        email = findViewById(R.id.editTextTextEmailAddress);    //email testbalk
+        password = findViewById(R.id.editTextTextPassword);        //password balk
+
+        sp = getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                emailStr = email.getText().toString();
+                passwordStr = password.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("email", emailStr);
+                editor.putString("password", passwordStr);
+                editor.commit();
+                Toast.makeText(LogIn.this, "Account details saved!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {              //dit regelt wat er gebeurd als je op Login knop klikt
             public void onClick(View v){
-                if(ed1.getText().toString().equals("test@outlook.com") &&    //uiteindelijk nog deze twee string veranderen naar de daadwerkelijke email en wachtwoord
-                        ed2.getText().toString().equals("password")){
+                SharedPreferences sp2 = getApplicationContext().getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
+                String emailSaved = sp2.getString("email", "");
+                String passwordSaved = sp2.getString("password", "");
+                //Toast.makeText(LogIn.this, passwordSaved, Toast.LENGTH_LONG).show();
+                if(email.getText().toString().equals(emailSaved) &&    //uiteindelijk nog deze twee string veranderen naar de daadwerkelijke email en wachtwoord
+                        password.getText().toString().equals(passwordSaved)){
                     Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
                 } else{
                     Toast.makeText(getApplicationContext(), "Wrong email/password...", Toast.LENGTH_SHORT).show();
